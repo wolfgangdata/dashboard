65e5ac6269287a09b45da5caa9db9ac8d5dfccd7
wolfgangdata <wolfgang2mairinger@gmail.com>
2018-03-05 11:30:33 -0500

    update

--- server.R
+++ server.R
@@ -6,66 +6,76 @@ library(plotly)
 library(leaflet)
 
 # data_col <- read.csv("data/data_col.csv", header=T, sep = ",")
-data_col <- read.csv("data/merged_col_01_02.csv", header=T, sep = ",")
+data_col <- read.csv("data/col_merged_01_02_03.csv", header=T, sep = ",")
 meta_deploy <- read.csv("data/meta_deployments.csv", header=T, sep = ",", stringsAsFactors = F)
 meta_deploy <- meta_deploy[-1, ]
 meta_col <- read.csv("data/meta_sampleID.csv", header=T, sep = ",", stringsAsFactors = F)
 
-# rename deployments
-data_col$deployment_name[data_col$deployment == "1"] <- meta_deploy$Country[meta_deploy$ID == "1"]
-data_col$deployment_name[data_col$deployment == "2"] <- meta_deploy$Country[meta_deploy$ID == "2"]
+# # rename deployments
+# data_col$deployment_name[data_col$deployment == "1"] <- meta_deploy$Country[meta_deploy$ID == "1"]
+# data_col$deployment_name[data_col$deployment == "2"] <- meta_deploy$Country[meta_deploy$ID == "2"]
 
 # add sample names and variables
-data_col <- left_join(data_col, meta_col, by = c("col_sample_type" = "ID"))
+# data_col <- left_join(data_col, meta_col, by = c("col_sample_type" = "ID"))
 
-deployments <- sort(unique(as.character(data_col$deployment_name)))
-deployments1 <- sort(unique(as.character(data_col$deployment_name)))
+# deployments <- sort(unique(as.character(data_col$deployment_name)))
+# deployments1 <- sort(unique(as.character(data_col$deployment_name)))
 
 
 
 server <- function(input, output, session) {
         
-        session$onSessionEnded(function() {
-                stopApp()
-        })
+        # session$onSessionEnded(function() {
+        #         stopApp()
+        # })
         
-        data_col_depl <- data.frame(table(data_col$deployment))
-        data_col_depl$Var1 <- as.numeric(as.character(data_col_depl$Var1))
-        data_col_depl <- left_join(data_col_depl, meta_deploy[, 1:2], by = c("Var1" = "ID"))
         
-        observe({
-                updateRadioButtons(session, "deployments", choices=deployments)
-        })
+        data_col <- left_join(data_col, meta_col[, 1:2], by = c("col_sample_type" = "ID"))
         
         observe({
-                updateRadioButtons(session, "deployments1", choices=deployments)
+                updateRadioButtons(session, "deployments", choices=c("Cambodia" = 1,
+                                                                     "Bangladesh" = 2,
+                                                                     "Ghana" = 3))
         })
         
         
+# **************************************************************************************************          
         output$hist <- renderPlotly({
-                dfhist <- count(data_col[data_col$deployment_name == input$deployments, ], col_sample_type)
-
-                dfhist2 <- as.data.frame(matrix(nrow = 10, ncol = 0))
-                dfhist2$col_sample_type <- 1:10
-                dfhist <- left_join(dfhist2, dfhist, by = "col_sample_type")
-                dfhist$n[is.na(dfhist$n)] <- 0
-                dfhist <- left_join(dfhist, meta_col, by = c("col_sample_type" = "ID"))
-                dfhist$sample_type_name <- factor(dfhist$sample_type_name, levels = unique(dfhist$sample_type_name)[order(dfhist$col_sample_type)])
-                
-                p <- plot_ly(dfhist, x = ~sample_type_name, y = ~n, type = 'bar') %>%
+                # dfhist <- count(data_col[data_col$deployment_name == input$deployments, ], col_sample_type)
+                # 
+                # dfhist2 <- as.data.frame(matrix(nrow = 10, ncol = 0))
+                # dfhist2$col_sample_type <- 1:10
+                # dfhist <- left_join(dfhist2, dfhist, by = "col_sample_type")
+                # dfhist$n[is.na(dfhist$n)] <- 0
+                # dfhist <- left_join(dfhist, meta_col, by = c("col_sample_type" = "ID"))
+                # dfhist$sample_type_name <- factor(dfhist$sample_type_name, levels = unique(dfhist$sample_type_name)[order(dfhist$col_sample_type)])
+                # 
+                p <- plot_ly(data_col[data_col$dply_num == input$deployments, ], 
+                             x = ~col_sample_type, 
+                             type = 'histogram') %>%
                         layout(showlegend = F,
                                xaxis = list(range = c(0,10),
                                             title = "")) 
                 
-                # p <- ggplot(data_col[data_col$deployment == input$deployments, ], aes(x=as.factor(col_sample_type))) +
-                #         geom_bar(stat="count") +
-                #         theme_light()
-                
-                
-          })
-        
-        output$table <- renderTable(as.data.frame(count(data_col[data_col$deployment_name == input$deployments1, ], 
+                })
+# **************************************************************************************************                                
+        output$map <- renderLeaflet(
+                leaflet(meta_deploy) %>% 
+                        addTiles() %>% 
+                        addMarkers(lng = ~long,
+                                   lat = ~lat, 
+                                   label = paste0(meta_deploy$City, ", ", meta_deploy$Country),
+                                   popup = ~Country,
+                                   options = markerOptions(draggable = F, riseOnHover = TRUE))
+        )
+# **************************************************************************************************                                
+        output$table <- renderTable(as.data.frame(count(data_col[data_col$dply_num == input$deployments, ], 
                                                         sample_type_name)))
+        
+        data_col_depl <- data.frame(table(data_col$dply_num))
+        data_col_depl$Var1 <- as.numeric(as.character(data_col_depl$Var1))
+        data_col_depl <- left_join(data_col_depl, meta_deploy[, 1:2], by = c("Var1" = "ID"))
+        
         
         output$testpie <- renderPlotly(
                 pie <- plot_ly(data_col_depl, values = ~Freq, labels = ~Country, type = "pie")
--- ui.R
+++ ui.R
@@ -8,62 +8,79 @@ library(plotly)
 dashboardPage(
         dashboardHeader(title = "SaniPath Dashboard"),
         dashboardSidebar(sidebarMenu(
+                menuItem("Home", tabName = "tabhome", icon = icon("home")),
                 menuItem("Map", tabName = "tabmap", icon = icon("map-marker")),
                 menuItem("Samples", tabName = "tabcol", icon = icon("bar-chart")),
                 menuItem("Surveys", tabName = "tabsurv", icon = icon("bar-chart")),
-                menuItem("Drinking Water", tabName = "tabboil", icon = icon("pie-chart")),
+                menuItem("Drinking Water", tabName = "tabboil", icon = icon("tint")),
                 menuItem("Latrines", tabName = "tablatrine", icon = icon("pie-chart"))
         )
         ),
         dashboardBody(
                 tabItems(
 # **************************************************************************************************
-                        # Tab 1 map
-                        tabItem(tabName = "tabmap",
+                        # Tab 1 home
+                        tabItem(tabName = "tabhome",
                                 HTML('<center><img src="sanipath_logo3.jpg" width="300" ></center>'),
                                 h2("Overview"),
                                 p("Here is an overview of the most recent SaniPath deployments."),
-                                p("MAP goes here")
+                                
+                                column(width = 4,
+                                       infoBox("Total Number of Deployments", nrow(meta_deploy),
+                                               width = NULL, color = "yellow", icon = icon("list")),
+                                
+                                       infoBox("Total Samples Collected", sum(data_col_depl$Freq), 
+                                               width = NULL, icon = icon("list"))
+                                
+                                )
+                                ),
+                        
+# **************************************************************************************************
+                        # Tab 2 map
+                        tabItem(tabName = "tabmap",
+                                h2("Map"),
+                                p("Previous Sanipath Deployments"),
+                                leafletOutput("map")
                                 ),
 # **************************************************************************************************                        
-                        # Tab 2 sample collection
+                        # Tab 3 sample collection
                         tabItem(tabName = "tabcol",
                                 h2("Information about samples goes here"),
                                 fluidRow(
                                         box(title = "Deployment", status = "info", solidHeader = TRUE, 
                                             radioButtons("deployments", "Deployments",
-                                                         choices = "")
-                                        ),
+                                                         choices = "")),
+                                        
+                                        box(title = "Samples collected per deployment", status = "info", solidHeader = TRUE,
+                                            plotlyOutput("testpie", height = 250)
+                                        )
+                                        
+                                ),
+                                fluidRow(
                                         box(title = "Histogram", status = "primary", solidHeader = TRUE,
                                             collapsible = FALSE,
                                             plotlyOutput("hist", height = 250)
-                                        )
-                                ),
-                                fluidRow(
-                                        box(title = "Test Pie", status = "info", solidHeader = TRUE,
-                                            "Percentage of samples collected per deployment",
-                                            plotlyOutput("testpie", height = 250)
                                         ),
-                                        box(title = "Table", status = "primary", solidHeader = FALSE,
+                                        box(title = "Table", status = "primary", solidHeader = TRUE,
                                             collapsible = FALSE,
-                                            radioButtons("deployments1", "Deployments",
-                                                         choices = ""),
                                             tableOutput("table")
                                         )
                                 
-                                )),
+                                )
+                                
+                                ),
 # **************************************************************************************************                        
-                        # Tab 3 suvey
+                        # Tab 4 suvey
                         tabItem(tabName = "tabsurv",
                                 h2("Information about surveys goes here")
                                 ),
 # **************************************************************************************************                        
-                        # Tab 4 suvey
+                        # Tab 5 suvey
                         tabItem(tabName = "tabboil",
                                 h2("Information about boiling drinking water goes here")
                         ), 
 # **************************************************************************************************                        
-                        # Tab 5 suvey
+                        # Tab 6 suvey
                         tabItem(tabName = "tablatrine",
                                 h2("Information about private latrine goes here")
                         )
